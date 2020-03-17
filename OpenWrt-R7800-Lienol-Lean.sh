b4e2ec38a5ec7b1ed7000ba24a7c14cd8204d218
ClayMoreBoy <ClayMoreBoy@users.noreply.github.com>
2020-03-17 23:26:09 +0800

    Update OpenWrt-R7800-Lienol-Lean.sh

--- OpenWrt-R7800-Lienol-Lean.sh
+++ OpenWrt-R7800-Lienol-Lean.sh
@@ -8,16 +8,11 @@
 #=================================================
 
 # 定制默认IP
-sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
-
-curl -fsSL https://github.com/ClayMoreBoy/OpenWrt-R7800-Actions/raw/master/mac80211.sh >> ~/package/kernel/mac80211/files/lib/wifi/mac80211.sh
-curl -fsSL https://github.com/ClayMoreBoy/OpenWrt-R7800-Actions/raw/master/mac80211.sh >> package/kernel/mac80211/files/lib/wifi/mac80211.sh
-
-# sed -i 's/ v2ray//g' target/linux/ipq806x/Makefile
+# sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 
 # WIFI名为MAC后六位
-# rm -f package/kernel/mac80211/files/lib/wifi/mac80211.sh
-# cp ../mac80211.sh package/kernel/mac80211/files/lib/wifi/mac80211.sh
+# rm -rf package/kernel/mac80211/files/lib/wifi/mac80211.sh
+# curl package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
 # uci set wireless.@wifi-iface[0].ssid=OpenWrt_$(cat /sys/class/ieee80211/phy0/macaddress|awk -F ":" '{print $4""$5""$6 }'| tr a-z A-Z)
 # uci commit
