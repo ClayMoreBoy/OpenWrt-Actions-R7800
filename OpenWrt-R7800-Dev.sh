5e201eb2339834307782e25bd57a0d823912632c
ClayMoreBoy <ClayMoreBoy@users.noreply.github.com>
2020-03-18 03:01:31 +0800

    Update OpenWrt-R7800-Dev.sh

--- OpenWrt-R7800-Dev.sh
+++ OpenWrt-R7800-Dev.sh
@@ -9,15 +9,22 @@
 
 # 定制默认IP
 sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
-sed -i 's/root::0:0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/g' /etc/shadow
-sed -i 's/lienol/ClayMore/g' /etc/opkg/distfeeds.conf
 
+# root默认密码password
+sed -i 's/root::0:0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/g' /etc/shadow
+sed -i 's/lienol//g' /etc/opkg/distfeeds.conf
+
+# 删除状况页upnp和ddns状态模块
 rm -f /usr/lib/lua/luci/view/admin_status/index/upnp.htm
 rm -f /usr/lib/lua/luci/view/admin_status/index/ddns.htm
 
 # WIFI名为MAC后六位
 rm -rf package/kernel/mac80211/files/lib/wifi/mac80211.sh
 cp -f ../mac80211.sh package/kernel/mac80211/files/lib/wifi/
+
+#替换banner
+rm -rf package/base-files/files/etc/banner
+cp -f ../banner package/base-files/files/etc/
 
 # 增加制作人
 # sed -i "s/echo \"DISTRIB_DESCRIPTION='OpenWrt SNAPSHOT '\"/echo \"DISTRIB_DESCRIPTION='OpenWrt SNAPSHOT Compiled by ClayMoreBoy '\"/g" package/default-settings/files/zzz-default-settings
