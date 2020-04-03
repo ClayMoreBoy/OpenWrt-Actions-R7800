cc55b1905010498570cdee3b531808500ffa1ef3
ClayMoreBoy <ClayMoreBoy@users.noreply.github.com>
2020-04-04 00:43:55 +0800

    Update OpenWrt-R7800-Lean.sh

--- OpenWrt-R7800-Lean.sh
+++ OpenWrt-R7800-Lean.sh
@@ -14,7 +14,7 @@ sed -i 's/ +luci-theme-bootstrap//g' feeds/luci/collections/luci/Makefile
 rm -rf package/kernel/mac80211/files/lib/wifi/mac80211.sh
 cp -f ../mac80211.sh package/kernel/mac80211/files/lib/wifi/
 
-#替换banner
+# 替换banner
 rm -rf package/base-files/files/etc/banner
 cp -f ../banner package/base-files/files/etc/
 
@@ -25,10 +25,10 @@ cp -f ../zzz-default-settings package/lean/default-settings/files/
 # 添加温度显示(By YYiiEt)
 sed -i 's/or "1"%>/or "1"%> ( <%=luci.sys.exec("expr `cat \/sys\/class\/thermal\/thermal_zone0\/temp` \/ 1000") or "?"%> \&#8451; ) /g' feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm
 
-#修改固件生成名字,增加当天日期(by:左右）
+# 修改固件生成名字,增加当天日期(by:左右）
 sed -i 's/IMG_PREFIX:=$(VERSION_DIST_SANITIZED)/IMG_PREFIX:=ClayMoreBoy-$(shell date +%Y%m%d)-$(VERSION_DIST_SANITIZED)/g' include/image.mk
 
-#修改版本号
+# 修改版本号
 sed -i 's/V2020/V$(date "+%Y.%m.%d")/g' package/lean/default-settings/files/zzz-default-settings
 
 # 添加第三方软件包
