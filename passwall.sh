21e2c09df9d6349cd0ddcf76e93693fdf03a5b43
ClayMoreBoy <claymor3.sky@gmail.com>
2020-04-07 05:21:44 +0800

    Create passwall.sh

---
+++ passwall.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+echo "src-git lienol https://github.com/Lienol/openwrt-package" >> feeds.conf.default
+./scripts/feeds clean
+./scripts/feeds update -a
+rm -rf feeds/lienol/lienol/v2ray
+rm -rf feeds/lienol/lienol/openssl1.1
+rm -rf feeds/lienol/lienol/trojan
+rm -rf feeds/lienol/lienol/ipt2socks
+rm -rf feeds/lienol/lienol/shadowsocks-libev-new
+rm -rf feeds/lienol/lienol/shadowsocksr-libev
+rm -rf feeds/lienol/lienol/pdnsd-alt
+rm -rf feeds/lienol/package/verysync
+rm -rf feeds/lienol/lienol/luci-app-verysync
+rm -rf package/lean/kcptun
+rm -rf package/lean/luci-app-kodexplorer
+rm -rf package/lean/luci-app-pppoe-relay
+rm -rf package/lean/luci-app-pptp-server
+rm -rf package/lean/luci-app-v2ray-server
+./scripts/feeds install -a
