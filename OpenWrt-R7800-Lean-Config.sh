2554ab304ffaf90a71aeb05c590a0b6c84f07765
ClayMoreBoy <claymor3.sky@gmail.com>
2020-04-09 22:47:51 +0800

    Update OpenWrt-R7800-Lean-Config.sh

--- OpenWrt-R7800-Lean-Config.sh
+++ OpenWrt-R7800-Lean-Config.sh
@@ -57,6 +57,13 @@ touch ./.config
 # 
 
 # 编译R7800固件:
+#cat >> .config <<EOF
+#CONFIG_TARGET_ipq806x=y
+#CONFIG_TARGET_ipq806x_generic=y
+#CONFIG_TARGET_ipq806x_generic_DEVICE_netgear_r7800=y
+#EOF
+
+# 编译R7800固件:
 cat >> .config <<EOF
 CONFIG_TARGET_ipq806x=y
 CONFIG_TARGET_ipq806x_DEVICE_netgear_r7800=y
