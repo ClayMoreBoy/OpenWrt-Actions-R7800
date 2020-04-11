840451dd7edef24ab431dfaba03c541283aff92d
ClayMoreBoy <claymor3.sky@gmail.com>
2020-04-12 04:57:54 +0800

    Update OpenWrt-R7800-Snapshot.sh

--- OpenWrt-R7800-Snapshot.sh
+++ OpenWrt-R7800-Snapshot.sh
@@ -31,19 +31,19 @@ rm -rf include/target.mk
 cp -f ../target.mk include/
 
 # 添加温度显示(By YYiiEt)
-sed -i 's/or "1"%>/or "1"%> ( <%=luci.sys.exec("expr `cat \/sys\/class\/thermal\/thermal_zone0\/temp` \/ 1000") or "?"%> \&#8451; ) /g' feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm
+# sed -i 's/or "1"%>/or "1"%> ( <%=luci.sys.exec("expr `cat \/sys\/class\/thermal\/thermal_zone0\/temp` \/ 1000") or "?"%> \&#8451; ) /g' feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm
 
 # 修改固件生成名字,增加当天日期(by:左右）
 sed -i 's/IMG_PREFIX:=$(VERSION_DIST_SANITIZED)/IMG_PREFIX:=ClayMoreBoy-$(shell date +%Y%m%d)-$(VERSION_DIST_SANITIZED)/g' include/image.mk
 
 # 修改版本号
-sed -i 's/V2020/V$(date "+%Y.%m.%d")/g' package/lean/default-settings/files/zzz-default-settings
+# sed -i 's/V2020/V$(date "+%Y.%m.%d")/g' package/lean/default-settings/files/zzz-default-settings
 
 # 切换
 sed -i 's/Lean/Snapshot/g' package/base-files/files/etc/banner
 
 # 修改版本号
-sed -i 's/V2020/V$(shell date +%Y.%m.%d)/g' package/base-files/files/etc/banner
+# sed -i 's/V2020/V$(shell date +%Y.%m.%d)/g' package/base-files/files/etc/banner
 
 # 创建自定义配置文件 - OpenWrt-R7800
 
