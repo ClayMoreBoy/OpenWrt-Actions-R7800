c2784886607b29ea4db08f04b747f3d84a5cf553
ClayMoreBoy <claymor3.sky@gmail.com>
2020-04-22 12:50:37 +0800

    Update passwall.sh

--- passwall.sh
+++ passwall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "src-git helloworld https://github.com/fw876/helloworld" >> feeds.conf.default
-echo "src-git lienol https://github.com/Lienol/openwrt-package" >> feeds.conf.default
+echo "src-git lienol https://github.com/ClayMoreBoy/openwrt-package" >> feeds.conf.default
 ./scripts/feeds clean
 ./scripts/feeds update -a
 rm -rf feeds/lienol/lienol/v2ray
