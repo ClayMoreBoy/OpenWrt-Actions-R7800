9b2c71015d2133c816ce98da18f638037817a328
ClayMoreBoy <claymor3.sky@gmail.com>
2020-07-22 12:13:46 +0800

    Update OpenWrt-R7800-Lean.sh

--- OpenWrt-R7800-Lean.sh
+++ OpenWrt-R7800-Lean.sh
@@ -29,7 +29,8 @@ sed -i 's/or "1"%>/or "1"%> ( <%=luci.sys.exec("expr `cat \/sys\/class\/thermal\
 sed -i 's/IMG_PREFIX:=$(VERSION_DIST_SANITIZED)/IMG_PREFIX:=ClayMoreBoy-$(shell date +%Y%m%d)-$(VERSION_DIST_SANITIZED)/g' include/image.mk
 
 # 修改版本号
-sed -i 's/V2020/V$(date "+%Y.%m.%d")/g' package/lean/default-settings/files/zzz-default-settings
+cid=$(date "+%Y-%m-%d")
+sed -i 's/R2020/R[${cid}]/g' package/lean/default-settings/files/zzz-default-settings
 
 # 修改版本号
 sed -i 's/V2020/V${{ env.DATE }}/g' package/base-files/files/etc/banner
